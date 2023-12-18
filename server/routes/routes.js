74a883ce5eb3391097139beb84fa26371f331f7c
Mario Perhat <148371794+Mariocci@users.noreply.github.com>
2023-12-18 18:31:00 +0100

    Add files via upload

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -84,6 +84,385 @@ router.get('/create-files', async (req, res) => {
   }
 });
 
+//GET za cijelu kolekciju podataka
+router.get('/api/games', async (req, res) => {
+  try {
+    const query = `
+      SELECT
+        jsonb_build_object(
+          'ime_igre', igra.ime,
+          'ime_izdavac', igra.ime_izdavac,
+          'developeri', jsonb_agg(jsonb_build_object('developer', developer_igra.developer, 'godina_osnutka', developer.godina_osnutka)),
+          'datum_izdanja', igra.datum_izdanja,
+          'cijena', igra.cijena,
+          'zanr', igra.zanr,
+          'predvidjeni_br_sati_za_igranje', igra.predvidjeni_br_sati_za_igranje,
+          'fransiza', igra.fransiza,
+          'ocjena', igra.ocjena,
+          'singleplayer', igra.singleplayer,
+          'multiplayer', igra.multiplayer
+        ) AS game_data
+      FROM igra
+      LEFT JOIN developer_igra ON igra.ime = developer_igra.ime_igra
+      LEFT JOIN developer ON developer_igra.developer = developer.ime_developera
+      GROUP BY igra.ime, igra.ime_izdavac, igra.datum_izdanja, igra.cijena, igra.zanr, igra.predvidjeni_br_sati_za_igranje, igra.fransiza, igra.ocjena, igra.singleplayer, igra.multiplayer
+    `;
+
+    const result = await pool.query(query);
+    const jsonData = result.rows;
+
+    if (jsonData.length === 0) {
+      const response = {
+        status: 'OK',
+        message: 'No game objects found',
+        response: null,
+      };
+      res.status(200).json(response);
+    } else {
+      const response = {
+        status: 'OK',
+        message: 'Fetched game objects',
+        response: jsonData
+      };
+
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    const errorResponse = {
+      status: 'Error',
+      message: 'Internal Server Error',
+      response: null,
+    };
+    res.status(500).json(errorResponse);
+  }
+});
+
+
+//dobavi podatke o jednoj igri sa imenom
+router.get('/api/game/:ime_igre', async (req, res) => {
+  try {
+    const { ime_igre } = req.params;
+
+    const query = `
+    SELECT
+    jsonb_build_object(
+        'ime_igre', igra.ime,
+        'ime_izdavac', igra.ime_izdavac,
+        'developeri', jsonb_agg(jsonb_build_object('developer', developer_igra.developer, 'godina_osnutka', developer.godina_osnutka)),
+        'datum_izdanja', igra.datum_izdanja,
+        'cijena', igra.cijena,
+        'zanr', igra.zanr,
+        'predvidjeni_br_sati_za_igranje', igra.predvidjeni_br_sati_za_igranje,
+        'fransiza', igra.fransiza,
+        'ocjena', igra.ocjena,
+        'singleplayer', igra.singleplayer,
+        'multiplayer', igra.multiplayer
+    ) AS game_data
+    FROM igra
+    LEFT JOIN developer_igra ON igra.ime = developer_igra.ime_igra
+    LEFT JOIN developer ON developer_igra.developer = developer.ime_developera
+    WHERE igra.ime = $1
+    GROUP BY igra.ime, igra.ime_izdavac, igra.datum_izdanja, igra.cijena, igra.zanr, igra.predvidjeni_br_sati_za_igranje, igra.fransiza, igra.ocjena, igra.singleplayer, igra.multiplayer
+    `;
+    const result = await pool.query(query,[ime_igre]);
+    const jsonData = result.rows;
+
+    if (jsonData.length === 0) {
+      const response = {
+        status: 'Not Found',
+        message: 'No game found with name'+ime_igre,
+        response: null,
+      };
+      res.status(404).json(response);
+    } else {
+      const response = {
+        status: 'OK',
+        message: 'Fetched game',
+        response: jsonData
+      };
+
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    const errorResponse = {
+      status: 'Error',
+      message: 'Internal Server Error',
+      response: null,
+    };
+    res.status(500).json(errorResponse);
+  }
+});
+
+//delete jednog podatka na kolekciji
+router.delete('/api/game/:ime_igre', async (req, res) => {
+  try {
+    const { ime_igre } = req.params;
+
+    const checkQuery = 'SELECT COUNT(*) FROM igra WHERE ime = $1';
+    const checkResult = await pool.query(checkQuery, [ime_igre]);
+
+    if (checkResult.rows[0].count === '0') {
+      const response = {
+        status: 'Not found',
+        message: `No game found with name ${ime_igre}`,
+        response: null,
+      };
+      res.status(404).json(response);
+    } else {
+      const deleteQuery = 'DELETE FROM igra WHERE ime = $1';
+      await pool.query(deleteQuery, [ime_igre]);
+
+      const successResponse = {
+        status: 'OK',
+        message: `Deleted game with name ${ime_igre}`,
+        response: null,
+      };
+      res.status(200).json(successResponse);
+    }
+  } catch (error) {
+    console.error('Error deleting data:', error);
+    const errorResponse = {
+      status: 'Error',
+      message: 'Internal Server Error',
+      response: null,
+    };
+    res.status(500).json(errorResponse);
+  }
+});
+
+//put za promijenit cijenu igre
+router.put('/api/game/:ime_igre/:nova_cijena', async (req, res) => {
+  try {
+    const { ime_igre } = req.params;
+    const { nova_cijena } = req.params;
+
+
+  const checkQuery = 'SELECT COUNT(*) FROM igra WHERE ime = $1';
+    const checkResult = await pool.query(checkQuery, [ime_igre]);
+    if (isNaN(parseFloat(nova_cijena))) {
+      console.error('Error updating data:', error);
+      const errorResponse = {
+        status: 'Error',
+        message: 'Price should be a number',
+        response: null,
+      };
+      res.status(400).json(errorResponse);
+    }
+    else if (checkResult.rows[0].count === '0') {
+      const response = {
+        status: 'Not found',
+        message: `No game found with name ${ime_igre}`,
+        response: null,
+      };
+      res.status(404).json(response);
+    } else {
+      const updateQuery = 'UPDATE igra SET cijena = $2 WHERE ime = $1';
+      await pool.query(updateQuery, [ime_igre,nova_cijena]);
+      const successResponse = {
+        status: 'OK',
+        message: `Updated game price name ${ime_igre} to new price : ${nova_cijena}`,
+        response: null,
+      };
+      res.status(200).json(successResponse);
+    }
+  } catch (error) {
+    console.error('Error updating data:', error);
+    const errorResponse = {
+      status: 'Error',
+      message: 'Internal Server Error',
+      response: null,
+    };
+    res.status(500).json(errorResponse);
+  }
+});
+
+//post za dodati igru
+router.post('/api/game', async (req, res) => {
+  try {
+    const gameData = req.body;
+    let checkQuery = `SELECT COUNT(*) FROM igra WHERE ime = $1`;
+    const checkResult1 = await pool.query(checkQuery, [gameData.ime]);
+    checkQuery = `SELECT COUNT(*) FROM developer WHERE  developer.ime_developera = $1`;
+    const checkResult2 = await pool.query(checkQuery, [gameData.developer]);
+
+    
+    if (checkResult1.rows[0].count !== '0') {
+      const errorResponse = {
+        status: 'Error',
+        message: 'Game already exists',
+        response: null,
+      };
+      res.status(400).json(errorResponse);
+      return;
+    } 
+    else if (checkResult2.rows[0].count === '0') {
+      const errorResponse = {
+        status: 'Error',
+        message: 'Game developer is not in database',
+        response: null,
+      };
+      res.status(400).json(errorResponse);
+      return;
+    }
+
+    const insertQuery = 'INSERT INTO igra (ime, ime_izdavac, datum_izdanja, cijena, zanr, predvidjeni_br_sati_za_igranje, fransiza, ocjena, singleplayer, multiplayer) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)';
+    await pool.query(insertQuery, [
+      gameData.ime,
+      gameData.ime_izdavac,
+      gameData.datum_izdanja,
+      gameData.cijena,
+      gameData.zanr,
+      gameData.predvidjeni_br_sati_za_igranje,
+      gameData.fransiza,
+      gameData.ocjena,
+      gameData.singleplayer,
+      gameData.multiplayer
+  ]);
+
+    res.status(201).json({ message: 'Game added successfully', data: gameData });
+  } catch (error) {
+    console.error('Error updating data:', error);
+    const errorResponse = {
+      status: 'Error',
+      message: 'Internal Server Error',
+      response: null,
+    };
+    res.status(500).json(errorResponse);
+  }
+});
+
+//GET za developera
+router.get('/api/developer/:ime_developera', async (req, res) => {
+  try {
+    const { ime_developera } = req.params;
+
+    const query = `
+      SELECT
+        jsonb_build_object(
+          'ime_developera', developer.ime_developera,
+          'godina_osnutka', developer.godina_osnutka
+        ) AS developer_data
+      FROM developer
+      WHERE developer.ime_developera = $1
+    `;
+
+    const result = await pool.query(query, [ime_developera]);
+    const jsonData = result.rows;
+
+    if (jsonData.length === 0) {
+      const response = {
+        status: 'Not Found',
+        message: `No developer found with name ${ime_developera}`,
+        response: null,
+      };
+      res.status(404).json(response);
+    } else {
+      const response = {
+        status: 'OK',
+        message: 'Fetched developer',
+        response: jsonData,
+      };
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    console.error('Error retrieving developer data:', error);
+    const errorResponse = {
+      status: 'Error',
+      message: 'Internal Server Error',
+      response: null,
+    };
+    res.status(500).json(errorResponse);
+  }
+});
+
+//GET za free igre
+router.get('/api/freegames', async (req, res) => {
+  try {
+    const query = `
+      SELECT
+        jsonb_build_object(
+          'ime_igre', igra.ime,
+          'cijena', igra.cijena
+        ) AS game_data
+      FROM igra
+      WHERE igra.cijena = 0
+    `;
+
+    const result = await pool.query(query);
+    const jsonData = result.rows;
+
+    if (jsonData.length === 0) {
+      const response = {
+        status: 'OK',
+        message: 'No free games found',
+        response: null,
+      };
+      res.status(200).json(response);
+    } else {
+      const response = {
+        status: 'OK',
+        message: 'Fetched free games',
+        response: jsonData,
+      };
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    console.error('Error retrieving free games data:', error);
+    const errorResponse = {
+      status: 'Error',
+      message: 'Internal Server Error',
+      response: null,
+    };
+    res.status(500).json(errorResponse);
+  }
+});
+
+//GET za price range
+router.get('/api/pricerange/:minPrice/:maxPrice', async (req, res) => {
+  try {
+    const { minPrice, maxPrice } = req.params;
+
+    const query = `
+      SELECT
+        jsonb_build_object(
+          'ime_igre', igra.ime,
+          'cijena', igra.cijena
+        ) AS game_data
+      FROM igra
+      WHERE igra.cijena BETWEEN $1 AND $2
+    `;
+
+    const result = await pool.query(query, [minPrice, maxPrice]);
+    const jsonData = result.rows;
+
+    if (jsonData.length === 0) {
+      const response = {
+        status: 'OK',
+        message: 'No games found within the specified price range',
+        response: null,
+      };
+      res.status(200).json(response);
+    } else {
+      const response = {
+        status: 'OK',
+        message: 'Fetched games within the specified price range',
+        response: jsonData,
+      };
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    console.error('Error retrieving games within price range data:', error);
+    const errorResponse = {
+      status: 'Error',
+      message: 'Internal Server Error',
+      response: null,
+    };
+    res.status(500).json(errorResponse);
+  }
+});
+
 function convertToCsv(jsonData) {
   const header = Object.keys(jsonData[0]).join(',') + '\n';
   const rows = jsonData.map(obj => Object.values(obj).join(',') + '\n');
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const port = 8000;
 const routes = require('./routes/routes'); 
 
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', routes);
 
